6d9d3e1379544d231cbea09d5d27805706efa0f8
Tommi Kramer <kramer@uni-mannheim.de>
2014-08-10 14:03:40 +0200

    no real change (only cosmetiques)

--- SmartSource/SmartSource/SmartSourceAppDelegate.m
+++ SmartSource/SmartSource/SmartSourceAppDelegate.m
@@ -6,7 +6,7 @@
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
-#import "SmartSourceAppDelegate.h"
+#import "SmartSourceAppDelegate.h" 
 #import "SmartSourceSplitViewController.h"
 #import "Component+Factory.h"
 #import "SmartSourceFunctions.h"
